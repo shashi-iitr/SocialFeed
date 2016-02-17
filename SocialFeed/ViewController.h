b15da1f99b6cb111f6053bd99cac6abcbb83c2bb
shashi kumar <shashi@toppr.com>
2016-02-18 00:09:50 +0530

    Initial Commit

---
+++ SocialFeed/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SocialFeed
+//
+//  Created by shashi kumar on 18/02/16.
+//  Copyright © 2016 Shashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SocialFeed/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SocialFeed
+//
+//  Created by shashi kumar on 18/02/16.
+//  Copyright © 2016 Shashi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
